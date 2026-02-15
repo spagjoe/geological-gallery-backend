9f06ab3e9d40f37e23146e6c065eecc13b006158
spagjoe <amcavoy27@gmail.com>
2026-02-15 03:43:50 -0800

    CORS fix for vercel

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://geological-gallery-backend-production.up.railway.app/api',
+    'https://mycoolrocks.vercel.app/',
     'https://mycool.rocks' // Your custom domain
   ],
   credentials: true
